6731e83de7e689f3bf5d2e97e17d21ba99a7525a
Badru <badruakfm@gmail.com>
2022-12-12 10:17:54 +0700

    fix  something

--- models/index.js
+++ models/index.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
 const pg = require('pg')
+const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
